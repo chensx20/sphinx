02e3422e8199b962250848f0ae488dca7dff2a24
Casey Brooks <casey.brooks@users.noreply.github.com>
2026-08-04 17:14:34 +0000

    fix(directives): emit source-read for includes

--- sphinx/directives/other.py
+++ sphinx/directives/other.py
@@ -8,6 +8,7 @@ from docutils.parsers.rst import directives
 from docutils.parsers.rst.directives.admonitions import BaseAdmonition
 from docutils.parsers.rst.directives.misc import Class
 from docutils.parsers.rst.directives.misc import Include as BaseInclude
+from docutils.statemachine import StateMachine
 
 from sphinx import addnodes
 from sphinx.domains.changeset import VersionChange  # noqa: F401  # for compatibility
@@ -17,6 +18,7 @@ from sphinx.util import docname_join, logging, url_re
 from sphinx.util.docutils import SphinxDirective
 from sphinx.util.matching import Matcher, patfilter
 from sphinx.util.nodes import explicit_title_re
+from sphinx.util.osutil import os_path
 
 if TYPE_CHECKING:
     from docutils.nodes import Element, Node
@@ -369,6 +371,20 @@ class Include(BaseInclude, SphinxDirective):
     """
 
     def run(self) -> list[Node]:
+        def _insert_input(include_lines: list[str], path: str) -> None:
+            text = '\n'.join(include_lines[:-2])
+
+            docname = self.env.path2doc(os_path(path))
+            arg = [text]
+            self.env.app.events.emit('source-read', docname, arg)
+            text = arg[0]
+
+            include_lines = text.splitlines() + include_lines[-2:]
+            StateMachine.insert_input(self.state_machine, include_lines, path)
+
+        if self.env.app.events.listeners.get('source-read'):
+            self.state_machine.insert_input = _insert_input  # type: ignore[method-assign]
+
         if self.arguments[0].startswith('<') and \
            self.arguments[0].endswith('>'):
             # docutils "standard" includes, do not do path processing
