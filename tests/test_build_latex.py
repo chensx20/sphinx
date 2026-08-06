00048d0677d6a9af8965fe9a3a6f4d03d17758ca
Casey Brooks <casey.brooks@openai.com>
2026-08-06 18:43:25 +0000

    fix(latex): trim inline code wrapper whitespace

--- tests/test_build_latex.py
+++ tests/test_build_latex.py
@@ -1608,7 +1608,8 @@ def test_latex_code_role(app):
     content = (app.outdir / 'python.tex').read_text()
 
     common_content = (
-        r'\PYG{k}{def} '
+        r'\PYG{k}{def}'
+        r'\PYG{+w}{ }'
         r'\PYG{n+nf}{foo}'
         r'\PYG{p}{(}'
         r'\PYG{l+m+mi}{1} '
@@ -1623,7 +1624,7 @@ def test_latex_code_role(app):
         r'\PYG{p}{)}'
         r'\PYG{p}{:} '
         r'\PYG{k}{pass}')
-    assert (r'Inline \sphinxcode{\sphinxupquote{' + '\n' +
-            common_content + '\n}} code block') in content
+    assert (r'Inline \sphinxcode{\sphinxupquote{%' + '\n' +
+            common_content + '\n%}} code block') in content
     assert (r'\begin{sphinxVerbatim}[commandchars=\\\{\}]' +
             '\n' + common_content + '\n' + r'\end{sphinxVerbatim}') in content
